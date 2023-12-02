aeb715b9f2f4c237f5d5b15f140ccfe105cc0c17
pedro558 <pedroafonso109@gmail.com>
2023-12-02 18:03:53 -0300

    :sparkles: feat: create dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import Button from '@/components/ui/Button';
+
+interface pageProps {
+  
+}
+ 
+const page: FC<pageProps> = () => {
+  return (
+    <Button size='lg'>hello</Button>
+  );
+}
+ 
+export default page;
