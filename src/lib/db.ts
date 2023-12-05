32bc7b47df042b339b0e472ddce05cafd12a9a97
pedro558 <pedroafonso109@gmail.com>
2023-12-04 23:38:26 -0300

    :sparkles: feat: add PrismaClient as 2nd db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,20 @@
 import { Redis } from "@upstash/redis";
+import { PrismaClient } from '@prisma/client'
 
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
 
-export const db: Redis =  new Redis({
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton()
+export const dbPrisma = prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+
+export const dbRedis: Redis =  new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL || '',
   token: process.env.UPSTASH_REDIS_REST_TOKEN || '',
 });
