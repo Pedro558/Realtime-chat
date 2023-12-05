74b8d863497e8ff5a350771519bc0959da2431e1
pedro558 <pedroafonso109@gmail.com>
2023-12-04 23:37:36 -0300

    :sparkles: feat: add CredentialsProvider to authOptions

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,13 @@
 import { NextAuthOptions } from "next-auth";
-import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
-import { db } from "./db";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import { dbRedis, dbPrisma } from "./db";
+
+import { compare } from "bcrypt";
+
+import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
+//import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 function getGoogleCredentials() {
   const clientId = process.env.GOOGLE_CLIENT_ID;
@@ -18,21 +24,94 @@ function getGoogleCredentials() {
 }
 
 export const authOptions: NextAuthOptions = {
-  adapter: UpstashRedisAdapter(db),
+  adapter: UpstashRedisAdapter(dbRedis),
   session: {
     strategy: "jwt",
   },
   pages: {
-    signIn: "/login",
+    signIn: "/sign-in",
   },
   providers: [
     GoogleProvider({
       clientId: getGoogleCredentials().clientId,
       clientSecret: getGoogleCredentials().clientSecret,
     }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "johndoe@email.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          return null;
+        }
+
+        const existingUser = await dbPrisma.user.findUnique({
+          where: { email: credentials?.email },
+        });
+
+        if (!existingUser) {
+          return null;
+        }
+
+        const passwordMatch = await compare(
+          credentials.password,
+          existingUser.password
+        );
+
+        if (!passwordMatch) {
+          return null;
+        }
+
+        return {
+          id: `${existingUser.id}`,
+          username: existingUser.username,
+          email: existingUser.email,
+        };
+      },
+    }),
   ],
-  callbacks: {
-    async jwt({ token, user }) {
+  callbacks: 
+  {
+    async jwt({ token, user}){
+      
+      if(user){
+        return {
+          ...token,
+          username: user.username
+        }
+      }
+      
+      return token
+
+    },
+
+    async session({session, token}){
+      return{
+        ...session,
+        user: {
+          ...session.user,
+          username: token.username
+        }
+      }
+    },
+    redirect(){
+      return"/dashboard";
+    }
+  }
+};
+
+
+/** CALLBACKS FROM GOOGLE PROVIDER 
+  { async jwt({ token, user }) {
       const dbUser = (await db.get(`user:${token.id}`)) as User | null;
 
       if (!dbUser) {
@@ -60,5 +139,36 @@ export const authOptions: NextAuthOptions = {
     redirect(){
       return '/dashboard'
     }
-  },
-};
+  }
+ */
+
+  /** CALLBACKS FROM CREDENTIALS
+   {
+    async jwt({ token, user}){
+    
+      if(user){
+        return {
+          ...token,
+          username: user.username
+        }
+      }
+      
+      return token
+
+    },
+
+    async session({session, token}){
+      return{
+        ...session,
+        user: {
+          ...session.user,
+          username: token.username
+        }
+      }
+    },
+    redirect(){
+      return"/dashboard";
+    }
+  }
+
+  */
