7726e60f3410e4392d8fd5177aee7dd10751e976
pedro558 <pedroafonso109@gmail.com>
2023-12-04 23:32:59 -0300

    :sparkles: feat: add handler to GET and POST

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,7 @@
 import NextAuth from "next-auth/next";
 import { authOptions } from "@/lib/auth";
 
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST }
 export default NextAuth(authOptions)
